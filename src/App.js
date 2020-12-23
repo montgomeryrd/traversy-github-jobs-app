73daf9c14f38cd663df90116a8c08435c8b8e9f5
montgomeryrd <montgomery_rd@yahoo.com>
2020-12-22 16:35:09 -0800

    fetches data from github api, 18:25 into tutorial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-export const App = () => {
+import useFetchJobs from './utils/useFetchJobs';
+import { Container } from 'react-bootstrap';
+import { useState } from 'react';
+
+const App = () => {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page);
+
   return (
-    <div className="App">
-    </div>
+    <Container>
+      { loading && <h1>currently loading ∙ please wait</h1> }
+      { error && <h1>error loading ∙ try refreshing</h1> }
+      <h1>{ jobs.length }</h1>
+    </Container>
   );
 }
+
+export default App;
