cf24355ee5c5eebd61c28b2c851048175e47da1b
montgomeryrd <montgomery_rd@yahoo.com>
2020-12-23 21:52:33 -0800

    page pagination

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import useFetchJobs from './utils/useFetchJobs';
 import { Container } from 'react-bootstrap';
 import { useState } from 'react';
 
+import Job from './components/Jobs';
+import JobsPagination from './components/JobsPagination';
+
 const App = () => {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
-  const { jobs, loading, error } = useFetchJobs(params, page);
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
   return (
-    <Container>
-      { loading && <h1>currently loading ∙ please wait</h1> }
-      { error && <h1>error loading ∙ try refreshing</h1> }
-      <h1>{ jobs.length }</h1>
+    <Container className="my-4">
+      <h1>Github Jobs</h1>
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
+      { loading && <h1>loading ∙∙∙ please wait</h1> }
+      { error && <h1>error loading ∙∙∙ try refreshing</h1> }
+      { jobs.map(job => {
+        return <Job key={job.id} job={job} />      
+      })}
     </Container>
   );
 }
